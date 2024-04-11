44c3f0fe96c5c5cb02f6480247889a7f41166efd
manshi39 <manshipatel131019@gmail.com>
2024-04-11 08:50:57 +0530

    addvendor

---
+++ src/component/RajkotVendor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RajkotVendorPage() {
+  return (
+    <div>
+      <h2>Rajkot Vendor Page</h2>
+      {/* Add your vendor page content here */}
+    </div>
+  );
+}
+
+export default RajkotVendorPage;
